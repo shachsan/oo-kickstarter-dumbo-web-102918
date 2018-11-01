401077c7d21fa7d10d6a8bb8b1e0cacd5f59a18e
SANJAY K SAI <shachsan@hotmail.com>
2018-11-01 17:33:54 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
   def back_project(project_name)
     # project = Project.new(project_name)
     @backed_projects << project_name
-    Project.(self)
+    Project.add_backer(self)
 
   end
 
