9de25f6b70d551bb1339253f68a4273c419c1094
SANJAY K SAI <shachsan@hotmail.com>
2018-11-01 17:31:21 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
   def back_project(project_name)
     # project = Project.new(project_name)
     @backed_projects << project_name
-    @bname.self << @backers
+    @name.self << @backers
     binding.pry
     0
 
