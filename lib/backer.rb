d22a4bdd756a3196913fdc4d63825da6e71fb17b
SANJAY K SAI <shachsan@hotmail.com>
2018-11-01 17:42:50 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Backer
   # attr_reader :name
-  attr_accessor :backed_projects, :name, :
+  attr_accessor :backed_projects, :name, :backers
 
   def initialize(name)
     @name = name
